0891482373de145851f5f48eea85322f6d4f9fcf
GlauberBalsani <glauber_29@hotmail.com>
2023-01-25 17:26:34 -0300

    selection sort

--- src/com/selectionSort/SelectionSort.java
+++ src/com/selectionSort/SelectionSort.java
@@ -2,15 +2,20 @@ package com.selectionSort;
 
 public class SelectionSort {
 	public static void main(String[] args) {
+		int[] arr = {64, 25, 12, 22, 11};
+		selectionSort(arr);
+		System.out.println("Ordenando");
+		printArr(arr);
 		
 		
 		
 	}
 	
 	public static void selectionSort(int[] arr) {
-		for (int i = 0; i < arr.length -1; i ++) {
-			int minIndex = arr[i];
-			for (int j = i + 1; j < arr.length; j++) {
+		int n = arr.length;
+		for (int i = 0; i < n - 1; i ++) {
+			int minIndex = i;
+			for (int j = i + 1; j < n - 1; j++) {
 				if(arr[j] < arr[minIndex]) {
 					minIndex = j;
 					
@@ -20,6 +25,13 @@ public class SelectionSort {
 			int temp = arr[minIndex];
 			arr[minIndex] = arr[i];
 			arr[i] = temp;
+		}
+	}
+	
+	public static void printArr(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.println(arr[i] + " ");
+		System.out.println();
 		}
 	}
 
